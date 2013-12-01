fcc14a1fd60e3c90acc17efc2a3ae01a02f2ab70
Sara Gorecki <sgorecki@gmail.com>
2013-11-30 23:07:42 -0500

    correct variable names reflect aspects of song (title, artist, genre)

--- app.rb
+++ app.rb
@@ -7,10 +7,11 @@ songs_array = Dir.entries("data").select {|f| !File.directory? f}
 def song_list(array)
 	array.each do |song|
 		split_song = song.split(" - ")
-		name = split_song[0]
-		artist_genre = split_song[1].split(" [")
-		artist = artist_genre[0]
-		genre = artist_genre[1].chomp("].mp3")
+		song_artist = split_song[0]
+		name_genre = split_song[1].split(" [")
+		song_name = name_genre[0]
+		song_genre = name_genre[1].chomp("].mp3")
+		song_name = song_name.downcase.gsub(" ", "_")
 	end
 end
 
