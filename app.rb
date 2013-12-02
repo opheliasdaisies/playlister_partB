8ab9165c881b15f172f1a07bc5ca004be014b2c0
Sara Gorecki <sgorecki@gmail.com>
2013-12-01 23:49:30 -0500

    add ability to pull up song pages

--- app.rb
+++ app.rb
@@ -64,38 +64,44 @@ def return_or_create_artist(artist_name)
 	new_artist
 end
 
+
+
+
+
+
+
 #User Interface Methods
-def playlister
+def playlister(all_songs)
 	puts "Browse by artist or genre?"
 	artist_genre = gets.chomp.downcase
-	responses(artist_genre)
+	responses(all_songs, artist_genre)
 	if artist_genre == "genre"
-		list_genres
-		select_genre
+		list_genres(all_songs)
+		select_genre(all_songs)
 	else
 		puts "I did not understand that."
-		playlister
+		playlister(all_songs)
 	end
 end
 
-def list_artists
+def list_artists(all_songs)
 	puts "There are #{Artist.count} artists available. They are:"
 	artist_list = []
 	Artist.all.each do |artist|
 		artist_list << "#{artist.name} -- #{artist.songs_count} Songs"
 	end
 	puts artist_list
-	select_artist
+	select_artist(all_songs)
 end
 
-def select_artist
+def select_artist(all_songs)
 	puts "Select Artist"
 	artist_choice = gets.chomp.downcase
-	responses(artist_choice)
-	artist_match(response)
+	responses(all_songs, artist_choice)
+	artist_match(all_songs, response)
 end
 
-def artist_match(artist_choice)
+def artist_match(all_songs, artist_choice)
 	Artist.all.each do |artist|
 		if artist.name.downcase == artist_choice
 			puts "#{artist.name} - #{artist.songs_count} Songs"
@@ -106,7 +112,7 @@ def artist_match(artist_choice)
 	end
 end
 
-def list_genres
+def list_genres(all_songs)
 	genres_sorted = Genre.all.sort_by do |genre| 
 		genre.songs.length
 	end
@@ -115,14 +121,14 @@ def list_genres
 	end
 end
 
-def select_genre
+def select_genre(all_songs)
 	puts "Select Genre"
 	genre_choice = gets.chomp.downcase
-	responses(genre_choice)
-	genre_match(genre_choice)
+	responses(all_songs, genre_choice)
+	genre_match(all_songs, genre_choice)
 end
 
-def genre_match(genre_choice)
+def genre_match(all_songs, genre_choice)
 	Genre.all.each do |genre|
 		if genre_choice == genre.name.downcase
 			puts "The #{genre.name.capitalize} genre has #{genre.songs.length} Songs and #{genre.artists.length} Artists:"
@@ -133,41 +139,51 @@ def genre_match(genre_choice)
 	end
 end
 
-def song_match(song_choice)
-
+def song_match(all_songs, song_choice)
+	all_songs.each do |song|
+		if song.name.downcase == song_choice
+			puts "#{song.name}"
+			puts "Artist: #{song.artist.name}"
+			puts "Genre: #{song.genre.name.capitalize}"
+		end
+	end
 end
 
-def responses(response)
+def responses(all_songs, response)
 	if response == "exit"
 		puts "Goodybe."
 		exit
 	elsif response == "help"
-		help		
+		help(all_songs)		
 	elsif response == "list artists" || response == "artist"
-		list_artists
+		list_artists(all_songs)
 	elsif response == "list genres" || response == "genre"
-		list_genres
+		list_genres(all_songs)
 	elsif response == "select artist"
-		select_artist
+		select_artist(all_songs)
 	elsif response == "select genre"
-		select_genre
+		select_genre(all_songs)
 	else
-		artist_match(response)
-		genre_match(response)
-		puts "Please make a new selection."
+		artist_match(all_songs, response)
+		genre_match(all_songs, response)
+		song_match(all_songs, response)
+		puts "Make a selection."
 		new_response = gets.chomp
-		responses(new_response)
+		responses(all_songs, new_response)
 	end
 end
 
-def help
+def help(all_songs)
 	puts "'exit' exits program"
 	puts "'help' brings up a list of options"
 	puts "'list artists' lists all artists"
 	puts "'list genres' lists all genres"
 	puts "'select artist' allows you to select an artist"
 	puts "'select genre' allows you to select a genre"
+	puts "Typing the name of an artist, genre, or song will take you to the page for that item."
+	response = gets.chomp.downcase
+	responses(all_songs, response)
 end
 
-song_list = song_list(songs_array)
-playlister
+all_songs = song_list(songs_array)
+playlister(all_songs)
