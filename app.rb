cc98826dea567434d03cfbd7648e0b6754a483e5
Sara Gorecki <sgorecki@gmail.com>
2013-12-01 19:57:07 -0500

    break method up into smaller methods

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
-require "./lib/artist.rb"
-require "./lib/genre.rb"
-require "./lib/song.rb"
+require_relative "./lib/artist.rb"
+require_relative "./lib/genre.rb"
+require_relative "./lib/song.rb"
 
 songs_array = Dir.entries("data").select {|f| !File.directory? f}
 
@@ -10,27 +10,15 @@ def song_list(array)
 		song_artist = get_artist(song)
 		song_name = get_name(song)
 		song_genre = get_genre(song)
-		
-		new_song = song_name.downcase.gsub(" ", "_")
-		#create new song object
-		songs << new_song = Song.new
-		#add name to new song
-		new_song.name = song_name
-
-		#see if genre exits and make new genre
-		genre_exists = false
-		genre_exists(song_genre)
-		song_genre = Genre.new unless genre_exists
-		new_song.genre = song_genre
-
-		#see if artist exists and make new artist
-		artist_exists = false
-		artist_exists(song_artist)
-		song_artist = Artist.new unless artist_exists
-		new_song.artist = song_artist
-		song_artist.add_song(new_song)
+		new_song = create_song(song_name)		
+		songs << new_song
+		new_genre = create_genre(song_genre)
+		new_artist = create_artist(song_artist)
+		# new_song.genre = new_genre
 	end
+	songs
 end
+
 
 def get_artist(song)
 	split_song = song.split(" - ")
@@ -47,17 +35,43 @@ def get_genre(song)
 	split_song[1].split(" [")[1].chomp("].mp3")
 end
 
-def genre_exists(song_genre)
+def create_song(song)
+	new_song = song.downcase.gsub(" ", "_")
+	new_song = Song.new
+	new_song.name = song
+	new_song
+end
+
+def create_genre(genre)
+	return if genre_exists?(genre)
+	new_genre = Genre.new
+	new_genre.name = genre
+end
+
+def genre_exists?(song_genre)
 	Genre::GENRES.each do |genre|
-		genre_exists = true if genre.name == song_genre
+		genre.name == song_genre ? genre_exists = true : genre_exists = false
 	end
 end
 
-def artist_exists(song_artist)
+def create_artist(artist)
+	return if artist_exist?(artist)
+	new_artist = Artist.new
+	artist.name = artist
+end
+
+def artist_exist?(song_artist)
 	Artist::ARTISTS.each do |artist|
-		artist_exists = true if artist.name == song_artist
+		artist.name == song_artist ? artist_exists = true : artist_exists = false
 	end
 end
 
 
-song_list(songs_array)
+
+
+p song_list = song_list(songs_array)
+# song_list.each do |song|
+# 	p song.genre
+# end
+p Genre.all
+p Artist.all
